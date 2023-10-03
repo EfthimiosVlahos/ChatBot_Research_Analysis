4f6f3181a513491a747b6280b0d5c30ca05982f6
EfthimiosVlahos <efthimios.vlahos@stonybrook.edu>
2023-10-03 10:16:14 -0400

    updated main

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ if process_url_clicked:
         docs = text_splitter.split_documents(data)
 
         # create embeddings and save it to FAISS index
-        embeddings = OpenAIEmbeddings()
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
         vectorstore_openai = FAISS.from_documents(docs, embeddings)
         main_placeholder.text("Embedding Vector Started Building...✅✅✅")
         time.sleep(2)
